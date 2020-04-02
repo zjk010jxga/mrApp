21afa7dfec0c58db48db22f792b5e721bc47c13d
Tony <tony@gmail.com>
2020-04-02 16:13:46 +0800

    new job

---
+++ app/src/main/java/com/bsl/mrapp/Activity/Me/ComplaintsActivity.java
@@ -0,0 +1,15 @@
+package com.bsl.mrapp.Activity.Me;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bsl.mrapp.R;
+
+public class ComplaintsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_complaints);
+    }
+}
